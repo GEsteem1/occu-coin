30376eff113eabff6723ce4015050a8f98aa923a
electricSoda <justin.hua.ge@gmail.com>
2022-05-08 21:44:44 -0600

    Integrated Auth0

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const path = require('path')
 
+const dotenv = require("dotenv").config()
+const { auth } = require('express-openid-connect')
+const { requiresAuth } = require('express-openid-connect');
+
 const app = express()
 const PORT = process.env.port || 3000
 
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirname, '/public'));
 
-app.use("/app", require("./routes/app"))
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: 'http://localhost:3000/',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.BASE
+}
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config))
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+})
+
+app.get('/profile', requiresAuth(), (req, res) => {
+	res.send(JSON.stringify(req.oidc.user));
+  });
 
 app.get("/", (req, res) => {
 	res.render("index")
