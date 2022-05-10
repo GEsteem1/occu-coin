16b556557a6afc38170aa550456444ac699c9f69
electricSoda <justin.hua.ge@gmail.com>
2022-05-09 21:37:31 -0600

    Changed base url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.BASE
 }
